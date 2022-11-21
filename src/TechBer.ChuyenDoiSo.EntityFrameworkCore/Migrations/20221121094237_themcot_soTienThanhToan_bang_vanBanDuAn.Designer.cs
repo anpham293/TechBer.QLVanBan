23588e4fb57878ebe359b9c3f8736a8d2aee6c8e
Hoàng Văn Thuận <hoangthuan.cnt57clc@gmail.com>
2022-11-21 16:45:10 +0700

    thêm cột SoTienThanhToan bảng VanBanDuAn

--- src/TechBer.ChuyenDoiSo.EntityFrameworkCore/Migrations/20221121094237_themcot_soTienThanhToan_bang_vanBanDuAn.Designer.cs
+++ src/TechBer.ChuyenDoiSo.EntityFrameworkCore/Migrations/20221121094237_themcot_soTienThanhToan_bang_vanBanDuAn.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechBer.ChuyenDoiSo.EntityFrameworkCore;
 
 namespace TechBer.ChuyenDoiSo.Migrations
 {
     [DbContext(typeof(ChuyenDoiSoDbContext))]
-    partial class ChuyenDoiSoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221121094237_themcot_soTienThanhToan_bang_vanBanDuAn")]
+    partial class themcot_soTienThanhToan_bang_vanBanDuAn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
