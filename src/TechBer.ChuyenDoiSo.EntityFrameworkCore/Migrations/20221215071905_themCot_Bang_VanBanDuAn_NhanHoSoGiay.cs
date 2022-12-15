81d65b56690400a337d5610920af5ad3a78d286f
Hoàng Văn Thuận <hoangthuan.cnt57clc@gmail.com>
2022-12-15 14:31:17 +0700

    thêm cột Bảng Văn bản dự án

---
+++ src/TechBer.ChuyenDoiSo.EntityFrameworkCore/Migrations/20221215071905_themCot_Bang_VanBanDuAn_NhanHoSoGiay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TechBer.ChuyenDoiSo.Migrations
+{
+    public partial class themCot_Bang_VanBanDuAn_NhanHoSoGiay : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TenNguoiGiaoHoSo",
+                table: "VanBanDuAns",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ThoiGianNhanHoSoGiay",
+                table: "VanBanDuAns",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TrangThaiNhanHoSoGiay",
+                table: "VanBanDuAns",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenNguoiGiaoHoSo",
+                table: "VanBanDuAns");
+
+            migrationBuilder.DropColumn(
+                name: "ThoiGianNhanHoSoGiay",
+                table: "VanBanDuAns");
+
+            migrationBuilder.DropColumn(
+                name: "TrangThaiNhanHoSoGiay",
+                table: "VanBanDuAns");
+        }
+    }
+}
