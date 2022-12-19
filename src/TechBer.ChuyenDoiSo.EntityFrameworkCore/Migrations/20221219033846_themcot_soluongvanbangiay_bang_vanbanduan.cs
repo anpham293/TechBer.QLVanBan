be4119e5af62c6d3fbf5a3790b77ffee274303b0
Hoàng Văn Thuận <hoangthuan.cnt57clc@gmail.com>
2022-12-19 10:41:05 +0700

    thêm cột Số lượng văn bản giấy

---
+++ src/TechBer.ChuyenDoiSo.EntityFrameworkCore/Migrations/20221219033846_themcot_soluongvanbangiay_bang_vanbanduan.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TechBer.ChuyenDoiSo.Migrations
+{
+    public partial class themcot_soluongvanbangiay_bang_vanbanduan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SoLuongVanBanGiay",
+                table: "VanBanDuAns",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoLuongVanBanGiay",
+                table: "VanBanDuAns");
+        }
+    }
+}
